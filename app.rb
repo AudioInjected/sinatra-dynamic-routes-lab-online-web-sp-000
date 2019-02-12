36c821038976934122e777fab4a825b6aec37dcf
Rafael Batista <rafaelb24@yahoo.com>
2019-02-12 20:33:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,9 +12,9 @@ class App < Sinatra::Base
   end 
   
   get '/say/:number/:phrase' do 
-    @number = params[:number].to_i
+    @number = params[:number]
     @phrase = params[:phrase]
-    @number.to_i
+    @number
     
   end 
 end
