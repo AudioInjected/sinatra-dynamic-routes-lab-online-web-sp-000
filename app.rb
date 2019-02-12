ba39d3f9578ca38d0a604d02de00f99d9095093b
Rafael Batista <rafaelb24@yahoo.com>
2019-02-12 20:53:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @array = []
     params.collect {|k, v| @array << v}
-    "#{@array.}"
+    "#{@array.join(" ")}."
     binding.pry
   end 
 end
